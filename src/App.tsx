f0151c1b5f9ba193cdcdc056cb56f2d57975c5c0
EgorZhuk <egorbug@gmail.com>
2023-01-08 23:45:43 +0300

    add setCount component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter';
+import {SetCounter} from './components/setCounter';
 
 function App() {
   return (
     <div className="App-header">
+
+      <SetCounter/>
       <Counter/>
     </div>
   );
---
+++ src/components/setCounter.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './counter.module.css';
+
+export const SetCounter = () => {
+  return (
+    <div className={s.counterWrapper}>
+      <div className={s.setCounterInputWrapper}>
+        <div className={s.inputContainer}>
+          <p>Max Value</p>
+          <input type='number'/>
+        </div>
+        <div className={s.inputContainer}>
+          <p>Start Value</p>
+          <input type='number'/>
+        </div>
+      </div>
+      <div className={s.counterButtons}>
+        <button onClick={x=>x} disabled={true}>Set</button>
+      </div>
+    </div>
+  );
+};
